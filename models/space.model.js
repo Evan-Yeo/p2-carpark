58ffd3dc6bb21ec716de27edbf6e4b1264b3e507
Evan-Yeo <me@evanyeo.com>
2020-07-23 18:21:18 +0800

    Carplate showing on the Lots, delete veh from the space(with ebere help)

--- models/space.model.js
+++ models/space.model.js
@@ -1,28 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const spaceSchema = Schema({
-    spaceNumber: {
-        type: String,
-        required: true,
-    },
-    spaceUsedBy: [{
-        type: String,
-        ref: "Vehicle",
-    }, ],
-    //     isFilled: {
-    //         type: Boolean,
-    //         default: false,
-    //     },
-
+	spaceNumber: {
+		type: String,
+		required: true,
+	},
+	spaceUsedBy: [
+		{
+			type: String,
+			ref: 'Vehicle',
+		},
+	],
+	//     isFilled: {
+	//         type: Boolean,
+	//         default: false,
+	//     },
 });
 
+spaceSchema.methods.getName = function (i) {
+	console.log('length', this.spaceUsedBy.length);
+	console.log('i', this.spaceUsedBy[i]);
+	// console.log(
+	// 	this.spaceUsedBy[i] ? this.spaceUsedBy[i].carPlate : 'No plates defined'
+	// );
+	return `Lot: ${this.spaceNumber}  Car Plate: ${
+		this.spaceUsedBy[i] ? this.spaceUsedBy[i].carPlate : 'No plates defined'
+	}`;
+};
 
-
-spaceSchema.methods.getName = function () {
-    return `Lot: ${this.spaceNumber}  Car Plate: ${this.spaceUsedBy.carPlate}`;
-}
-
-
-const Space = mongoose.model("Space", spaceSchema);
+const Space = mongoose.model('Space', spaceSchema);
 module.exports = Space;
--- routes/space.route.js
+++ routes/space.route.js
@@ -1,22 +1,19 @@
-const router = require("express").Router();
-const Space = require("../models/space.model");
-const Vehicle = require("../models/vehicle.model");
-const User = require("../models/user.model");
-
+const router = require('express').Router();
+const Space = require('../models/space.model');
+const Vehicle = require('../models/vehicle.model');
+const User = require('../models/user.model');
 
 // router.get("/spaces", (req, res) => {
 //     res.render("spaces/show");
 // });
-
-
 
 /* 
   @method GETT
   @route spaces/new
   @desc displays new spaces form
 */
-router.get("/new", (req, res) => {
-    res.render("spaces/new");
+router.get('/new', (req, res) => {
+	res.render('spaces/new');
 });
 
 /* 
@@ -24,39 +21,48 @@ router.get("/new", (req, res) => {
   @route spaces/new
   @desc creates new spaces
 */
-router.post("/new", (req, res) => {
-    let space = new Space(req.body);
+router.post('/new', (req, res) => {
+	let space = new Space(req.body);
 
-    space
-        .save()
-        .then(() => {
-            res.redirect("/spaces");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+	space
+		.save()
+		.then(() => {
+			res.redirect('/spaces');
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
-router.get("/", async (req, res) => {
-    try {
-        //get all spaces
-        let spaces = await Space.find()
-            .populate("spaceUsedBy");
+router.get('/', async (req, res) => {
+	try {
+		//get all spaces
+		let spaces = await Space.find().populate('spaceUsedBy');
+		console.log(spaces[1]);
+		//get all users
+		let users = await User.find();
 
-        //get all users
-        let users = await User.find();
+		//get all vehicles
+		let vehicles = await Vehicle.find();
 
-        //get all vehicles
-        let vehicles = await Vehicle.find();
+		res.render('spaces/show', {
+			spaces,
+			users,
+			vehicles,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
 
-        res.render("spaces/show", {
-            spaces,
-            users,
-            vehicles
-        });
-    } catch (error) {
-        console.log(error);
-    }
+router.post('/:id/vehicle/:vehicle_id', (req, res) => {
+	Space.findByIdAndUpdate(req.params.id, {
+		$pull: { spaceUsedBy: req.params.vehicle_id },
+	})
+		.then(() => {
+			res.redirect('/spaces');
+		})
+		.catch((err) => console.log(err));
 });
 
 module.exports = router;
